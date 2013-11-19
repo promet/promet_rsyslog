73a174356f9236598d17ba350382e46255c682e8
Greg Palmier <gregpalmier@gmail.com>
2013-11-19 12:34:56 -0600

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rsyslog-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rsyslog-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'rsyslog'
